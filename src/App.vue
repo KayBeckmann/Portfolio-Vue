cf4de0f1eeb60f9676d37e3fc6d7e2c2f13b510a
snuppedeluxe <snuppedeluxe@gmail.com>
2024-12-11 10:43:12 +0000

    Clean up

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
-<script setup>
-import { RouterView } from 'vue-router';
-import MenuComponent from './components/MenuComponent.vue';
-import FooterComponent from './components/FooterComponent.vue';
-</script>
-
 <template>
 <MenuComponent></MenuComponent>
 <RouterView></RouterView>
 <FooterComponent></FooterComponent>
 </template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+import MenuComponent from './components/MenuComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+</script>
 
 <style scoped>
 
