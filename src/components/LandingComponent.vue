fe2ec07147f86036e529f158b8666e68e096362f
snuppedeluxe <snuppedeluxe@gmail.com>
2024-12-13 06:27:27 +0000

    Style changes

--- src/components/LandingComponent.vue
+++ src/components/LandingComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div> 
+  <div class="content"> 
     <h1>{{ currentMessages.landingPage.title }}</h1> 
     <p>{{ currentMessages.landingPage.subtitle }}</p>
   </div>
@@ -11,3 +11,12 @@ import { useLanguageStore } from '../stores/language';
 const { currentMessages } = useLanguageStore(); 
 </script>
 
+<style scoped>
+.content{
+  height: calc(100dvw - var(--menu-height));
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
