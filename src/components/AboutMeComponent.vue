ddae36e202d165268eaa72769b83bf4ae1bfbaaf
snuppedeluxe <snuppedeluxe@gmail.com>
2024-12-16 13:25:26 +0000

    Changed text for translation

--- src/components/AboutMeComponent.vue
+++ src/components/AboutMeComponent.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1>AboutMe</h1>
+    <h2>{{ currentMessages.aboutMe.title }}</h2>
+<div>
+    {{ currentMessages.aboutMe.description }}
+</div>
+<div>
+    <div>
+        Bulb
+        {{ currentMessages.aboutMe.skill1 }}
+    </div>
+
+    <div>
+        Piece of puzzle
+        {{ currentMessages.aboutMe.skill2 }}
+    </div>
+</div>
 </template>
+
+<script setup lang="js">
+import { useLanguageStore } from '../stores/language';
+
+const { currentMessages } = useLanguageStore(); 
+</script>
