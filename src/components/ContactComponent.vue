d4c90220381237bfe96ffff4129d464257ba3c9b
snuppedeluxe <snuppedeluxe@gmail.com>
2024-12-17 08:41:36 +0000

    Contact-Component first informations

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -1,3 +1,17 @@
 <template>
-    <h1>CONTACT</h1>
+    <h1>{{ currentMessages.contact.title }}</h1>
+    <a href="mailto:contact@kay-beckmann.com">{{ currentMessages.contact.mail }}</a>
 </template>
+
+<script setup lang="js">
+import { useLanguageStore } from '../stores/language';
+
+const { currentMessages } = useLanguageStore(); 
+</script>
+
+<style lang="css" scoped>
+a{
+    text-decoration: none;
+    color: var(--text-color);
+}
+</style>
