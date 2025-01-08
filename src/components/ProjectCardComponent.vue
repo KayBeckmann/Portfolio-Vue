9b8e1247d56bce7a847f42839681627d2b229ef7
snuppedeluxe <snuppedeluxe@gmail.com>
2025-01-08 13:16:19 +0000

    add projectcards - no layout

---
+++ src/components/ProjectCardComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <h2>{{ props.project.title }}</h2>
+    <p>{{ props.project.description }}</p>
+    <a :href="props.project.url">{{ props.project.url }}</a>
+</template>
+
+<script setup lang="js">
+import { useLanguageStore } from '../stores/language';
+import { defineProps } from 'vue';
+
+const { currentMessages } = useLanguageStore(); 
+
+const props = defineProps({
+       project: {
+           type: Object,
+           required: true
+       }
+   });
+</script>
+
+<style lang="css" scoped>
+
+</style>
--- src/components/ProjekteComponent.vue
+++ src/components/ProjekteComponent.vue
@@ -1,3 +1,29 @@
 <template>
     <h1>Projekte</h1>
+
+    <ProjectCardComponent v-for="project in projects" :key="project.title" :project="project"></ProjectCardComponent>
 </template>
+
+<script setup lang="js">
+import { useLanguageStore } from '../stores/language';
+import ProjectCardComponent from './ProjectCardComponent.vue';
+
+let projects = [
+    {
+        "title":"Join",
+        "description":"Taskmanagement",
+        "url":"https://link-zu.projct"
+    },
+    {
+        "title":"KiTa",
+        "description":"Homepage Kindergarten",
+        "url":"https://link-zu.projct"
+    },
+]
+
+const { currentMessages } = useLanguageStore(); 
+</script>
+
+<style lang="css" scoped>
+
+</style>
