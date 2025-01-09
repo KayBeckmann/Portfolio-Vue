4d3e3e20457815ea6de3b2c3e7eea662043f099b
Kay <info@beckmann-md.de>
2025-01-09 20:27:50 +0100

    add SkillCardsComponent

---
+++ src/components/SkillCardComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="skill-card">
+    <img :src="getImageUrl(skill.img)" :alt="skill.title">
+    <p>{{ skill.title }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  skill: {
+    type: Object,
+    required: true
+  }
+});
+
+// Dynamische Funktion zum Auflösen des Bildpfads
+const getImageUrl = (img) => {
+  return new URL(`../assets/icons/${img}`, import.meta.url).href;
+};
+</script>
+
+<style scoped>
+.skill-card {
+  border: 1px solid #ddd;
+  padding: 20px;
+  margin: 10px 0;
+  border-radius: 15px;
+}
+
+.project-card:hover {
+  border: 5px solid #00f7f7;
+  margin: 6px 0;
+}
+</style>
--- src/components/SkillsComponent.vue
+++ src/components/SkillsComponent.vue
@@ -1,11 +1,47 @@
 <template>
-    <h1>{{ currentMessages.menu.skills }}</h1>
+    <div class="wrapper">
+        <h1>{{ currentMessages.menu.skills }}</h1>
+    </div>
+    <div class="content">
+        <SkillCardComponent v-for="skill in skills" :key="skill.title" :skill="skill"></SkillCardComponent>
+    </div>
 </template>
 
 <script setup lang="js">
 import { useLanguageStore } from '../stores/language';
+import SkillCardComponent from './SkillCardComponent.vue'
 
 const { currentMessages } = useLanguageStore(); 
+const skills = [
+    {
+        "title": "HTML",
+        "img":"html.png"
+    },
+    {
+        "title": "CSS",
+        "img":"css.png"
+    },
+    {
+        "title": "JavaScript",
+        "img":"javascript.png"
+    },
+    {
+        "title": "TypeScript",
+        "img":"typescript.png"
+    },
+    {
+        "title": "git",
+        "img":"git.png"
+    },
+    {
+        "title": "Materialdesign",
+        "img":"materialdesign.png"
+    },
+]
 </script>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.wrapper{
+  padding: var(--margin);
+}
+</style>
