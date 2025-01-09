0c230e12df7d49ac20ee416aef5e11c6d58cdb13
snuppedeluxe <snuppedeluxe@gmail.com>
2025-01-09 18:41:03 +0000

    Skills edited

--- src/components/SkillsComponent.vue
+++ src/components/SkillsComponent.vue
@@ -1,3 +1,11 @@
 <template>
-    <h1>Skills</h1>
+    <h1>{{ currentMessages.menu.skills }}</h1>
 </template>
+
+<script setup lang="js">
+import { useLanguageStore } from '../stores/language';
+
+const { currentMessages } = useLanguageStore(); 
+</script>
+
+<style lang="css" scoped></style>
