2688dd5f91ef4eebde34487de565ba8d3db95cc1
Kay <info@beckmann-md.de>
2025-01-15 20:51:30 +0100

    add background image

--- src/components/LandingComponent.vue
+++ src/components/LandingComponent.vue
@@ -14,9 +14,22 @@ const { currentMessages } = useLanguageStore();
 <style lang="css" scoped>
 .content {
   height: 100dvh;
+  width: 100dvw;
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  background-image: url(../assets/img/background.webp),
+  linear-gradient(to bottom, rgba(0, 190, 232, 0.4), rgba(0, 190, 232, 0.4));
+  background-blend-mode: multiply;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+@media screen and (max-width: 750px) {
+  .content{
+    width: 100dvw;
+  }
 }
 </style>
