9f1dedd4b960fdab60e70633169e539e7bbdf1bc
Kay <info@beckmann-md.de>
2025-01-17 10:54:30 +0100

    edit landingpage

--- src/components/LandingComponent.vue
+++ src/components/LandingComponent.vue
@@ -1,35 +1,130 @@
 <template>
+<div class="backgrounds">
+  <img class="background left" src="../assets/img/shadow_1.png" alt="" />
+  <img class="background right" src="../assets/img/shadow_2.png" alt="" />
+</div>
+<div class="wrapper-landing">
   <div class="content">
-    <h1>{{ currentMessages.landingPage.title }}</h1>
-    <p>{{ currentMessages.landingPage.subtitle }}</p>
+    <!-- <div class="senkrecht"> -->
+      <!-- <h1>{{ currentMessages.landingPage.intro }}</h1> -->
+    <!-- </div> -->
+    <div class="headline">
+      <h1>{{ currentMessages.landingPage.title }}</h1>
+      <h2>{{ currentMessages.landingPage.subtitle }}</h2>
+    </div>
   </div>
+
+  <button
+    class="primary">
+    Let's talk!
+  </button>
+</div>
+<div class="footer">
+  <div class="footer-left">
+    <hr />
+    <SocialmediaComponent></SocialmediaComponent>
+  </div>
+  <div class="footer-right pointer">
+    Scroll down
+    <img class="arrow" src="../assets/icons/arrow.png" alt="->" />
+  </div>
+</div>
+
 </template>
 
 <script setup lang="js">
 import { useLanguageStore } from '../stores/language';
+import SocialmediaComponent from './SocialmediaComponent.vue';
 
 const { currentMessages } = useLanguageStore();
 </script>
 
-<style lang="css" scoped>
-.content {
-  height: 100dvh;
-  width: 100dvw;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  background-image: url(../assets/img/background.webp),
-  linear-gradient(to bottom, rgba(0, 190, 232, 0.4), rgba(0, 190, 232, 0.4));
-  background-blend-mode: multiply;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
+<style lang="scss" scoped>
+.backgrounds {
+  overflow-x: hidden;
 }
 
-@media screen and (max-width: 750px) {
-  .content{
-    width: 100dvw;
+.wrapper-landing {
+  height: 100dvh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .senkrecht {
+    transform: rotate(-90deg);
+    font-size: clamp(0.6rem, -0.273rem + 4.36vw, 3rem);
   }
 }
+
+.headline {
+  h1 {
+    font-size: clamp(1rem, -1.545rem + 12.73vw, 8rem);
+    margin: 0;
+  }
+  h2 {
+    background: -webkit-linear-gradient(0deg, #1cdae6, #4b47ff);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-size: clamp(1rem, -0.091rem + 5.45vw, 4rem);
+    margin: 0;
+  }
+}
+
+.backgrounds {
+  overflow: hidden;
+}
+
+.background {
+  z-index: -4;
+  position: absolute;
+  height: 90%;
+  top: 15%;
+}
+
+.left {
+  left: -250px;
+}
+
+.right {
+  right: -400px;
+}
+
+.footer {
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.footer-left {
+  display: flex;
+
+  hr {
+    width: 6dvw;
+    margin-right: 15px;
+  }
+}
+
+.footer-right {
+  margin-bottom: 70px;
+  font-size: 1.2rem;
+  transform: rotate(90deg);
+  color: white;
+
+  img {
+    margin-left: 5px;
+    transform: rotate(-90deg);
+  }
+}
+
 </style>
