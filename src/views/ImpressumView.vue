c4e6d38f9fb1b053bcf2c475f918065656c73753
Kay <info@beckmann-md.de>
2025-02-17 20:41:13 +0100

    add impressum and dsgvo.

--- src/views/ImpressumView.vue
+++ src/views/ImpressumView.vue
@@ -1,3 +1,29 @@
 <template>
-    <h1>IMPRESSUM</h1>
+<h1 class="nomargin">Impressum</h1>
+    
+    <p>Kay Beckmann<br /> <br />Hauptstraße 28<br />23923 Menzendorf</p>
+    
+    <h2>Kontakt</h2>
+    <p>E-Mail: info@beckmann-md.de</p>
+    
+    <!-- <h2>Umsatzsteuer-ID</h2> -->
+    <!-- <p>Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; &sect; 27 a Umsatzsteuergesetz:<br />DE999999999</p> -->
+    
+    <h2>Redaktionell verantwortlich</h2>
+    <p>Kay Beckmann<br />Hauptstraße 28<br />23923 Menzendorf</p>
+    
+    <h2>EU-Streitschlichtung</h2>
+    <p>Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a
+        href="https://ec.europa.eu/consumers/odr/" target="_blank"
+        rel="noopener noreferrer">https://ec.europa.eu/consumers/odr/</a>.<br /> Unsere E-Mail-Adresse finden Sie oben im
+      Impressum.</p>
+    
+    <h2>Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle</h2>
+    <p>Wir nehmen an einem Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teil. Zust&auml;ndig ist die
+      Universalschlichtungsstelle des Zentrums f&uuml;r Schlichtung e.V., Stra&szlig;burger Stra&szlig;e 8, 77694 Kehl am
+      Rhein (<a href="https://www.verbraucher-schlichter.de" rel="noopener noreferrer"
+        target="_blank">https://www.verbraucher-schlichter.de</a>).</p>
+    
+      <p>Quelle: <a href="https://www.e-recht24.de">https://www.e-recht24.de</a></p>
+    <a href="/">Zurück</a>
 </template>
